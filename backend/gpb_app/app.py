0f1cc64b541e6a39bd6f72d9fabbf75d4deca35e
Synkied <synkx@hotmail.fr>
2018-03-02 17:35:49 +0100

    Adding files.

--- backend/gpb_app/app.py
+++ backend/gpb_app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__,
             static_folder="../../dist/static",
             template_folder="../../dist")
 
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}, r"/question/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "https://grandpybot-vue-ql.herokuapp.com/"}, r"/question/*": {"origins": "https://grandpybot-vue-ql.herokuapp.com/"}})
 
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
