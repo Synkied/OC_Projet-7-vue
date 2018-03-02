879f3b820217fabbdbc9979fc30dff33cf81c1bc
Synkied <synkx@hotmail.fr>
2018-03-02 17:31:04 +0100

    Adding files.

--- backend/gpb_app/views.py
+++ backend/gpb_app/views.py
@@ -17,7 +17,7 @@ def index():
 @app.route('/<path:path>')
 def catch_all(path):
     if app.debug:
-        return requests.get('http://localhost:8080/{}'.format(path)).text
+        return requests.get('https://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
 
 
