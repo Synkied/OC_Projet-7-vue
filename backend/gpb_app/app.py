5402847e8ebfef2b60ebb6ad789b69b8d3f23fc5
Synkied <synkx@hotmail.fr>
2018-03-15 12:30:09 +0100

    changes

--- backend/gpb_app/app.py
+++ backend/gpb_app/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}, r"/question/*": {"origins": "*"}})
 
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
